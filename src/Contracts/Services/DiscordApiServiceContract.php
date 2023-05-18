c034ad43ed9d9afb4f703aab60b20add6dc2bfa6
Brian <brian@steliosmedia.com>
2023-05-18 11:04:39 +0200

    Allow static data in array for richtext

--- src/Contracts/Services/DiscordApiServiceContract.php
+++ src/Contracts/Services/DiscordApiServiceContract.php
@@ -3,17 +3,16 @@ declare(strict_types=1);
 
 namespace Nwilging\LaravelDiscordBot\Contracts\Services;
 
-use Nwilging\LaravelDiscordBot\Support\Embed;
-
 interface DiscordApiServiceContract
 {
     public function sendTextMessage(string $channelId, string $message, array $options = []): array;
 
     /**
      * @param string $channelId
-     * @param Embed[] $embeds
+     * @param array[] $embeds
+     * @param array[] $components
      * @param array $options
-     * @return void
+     * @return array
      */
     public function sendRichTextMessage(string $channelId, array $embeds, array $components = [], array $options = []): array;
 }
